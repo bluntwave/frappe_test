d9f485534875a11a08487009fffebaa66cc8ba59
Kirill Ivanov <kivanov@express42.com>
2023-10-01 15:49:49 +0400

    add frappe folder

---
+++ .devcontainer/init.sh
@@ -0,0 +1,13 @@
+"/home/frappe/frappe-bench/env/bin/gunicorn", \
+  "--chdir=/home/frappe/frappe-bench/sites", \
+  "--bind=0.0.0.0:8000", \
+  "--threads=4", \
+  "--workers=2", \
+  "--worker-class=gthread", \
+  "--worker-tmp-dir=/dev/shm", \
+  "--timeout=120", \
+  "--preload", \
+  "--daemon", \
+  "frappe.app:application"
+
+nginx-entrypoint.sh
